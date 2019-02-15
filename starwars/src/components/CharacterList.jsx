00779a499d48c2102fbf67d9160eb27fdf73a685
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-02-15 11:51:31 -0500

    Add span to labels

--- starwars/src/components/CharacterList.jsx
+++ starwars/src/components/CharacterList.jsx
@@ -6,14 +6,30 @@ const CharacterList = ({ characters }) => {
     <div className="character-list">
       {characters.map(c => (
         <div key={c.name} className="character-list-item">
-          <div className="name">Name: {c.name}</div>
-          <div className="dob">DOB: {c.birth_year}</div>
-          <div className="eyes">Eye Color: {c.eye_color}</div>
-          <div className="gender">Gender: {c.gender}</div>
-          <div className="hair">Hair Color: {c.hair_color}</div>
-          <div className="height">Height: {c.height}</div>
-          <div className="mass">Mass: {c.mass}</div>
-          <div className="skin">Skin Color: {c.skin_color}</div>
+          <div className="name">
+            <span className="label">Name</span> {c.name}
+          </div>
+          <div className="dob">
+            <span className="label">DOB</span> {c.birth_year}
+          </div>
+          <div className="eyes">
+            <span className="label">Eye Color</span> {c.eye_color}
+          </div>
+          <div className="gender">
+            <span className="label">Gender</span> {c.gender}
+          </div>
+          <div className="hair">
+            <span className="label">Hair Color</span> {c.hair_color}
+          </div>
+          <div className="height">
+            <span className="label">Height</span> {c.height}
+          </div>
+          <div className="mass">
+            <span className="label">Mass</span> {c.mass}
+          </div>
+          <div className="skin">
+            <span className="label">Skin Color</span> {c.skin_color}
+          </div>
         </div>
       ))}
     </div>
