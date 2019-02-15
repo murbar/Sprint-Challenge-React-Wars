2ea8d1b53ed2ac110ad49cfdc90cfa5db9ddad84
Joel Bartlett <6668097+murbar@users.noreply.github.com>
2019-02-15 13:27:38 -0500

    Add classes for styling

--- starwars/src/components/Pagination.jsx
+++ starwars/src/components/Pagination.jsx
@@ -4,13 +4,13 @@ const Pagination = ({ next, prev, onPageChange }) => {
   return (
     <div className="pagination">
       {prev && (
-        <button title="Previous page" onClick={() => onPageChange(prev)}>
-          Previous Page
+        <button className="prev" title="Previous page" onClick={() => onPageChange(prev)}>
+          « Previous Page
         </button>
       )}
       {next && (
-        <button title="Next page" onClick={() => onPageChange(next)}>
-          Next Page
+        <button className="next" title="Next page" onClick={() => onPageChange(next)}>
+          Next Page »
         </button>
       )}
     </div>
